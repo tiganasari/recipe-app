19a160a5ed7e3c43ce58d0841d460bc98c034542
Tigana-Sari <tigana.sari@gmail.com>
2021-09-10 20:04:34 +0100

    set up API request

--- src/App.js
+++ src/App.js
@@ -10,21 +10,28 @@ const App = () => {
 
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  useEffect(() => {
-    console.log('effect has been run')
-  })
+  const [recipes, setRecipes] = useState([]);
 
-  const[counter, setCounter] = useState(0);
+  useEffect( () => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(exampleReq);
+    const data = await response.json();
+    setRecipes(data.hits);
+
+
+  }
 
   return (
     <div className="App">
      <form className="search-form">
        <input className="search-bar" type="text"></input>
-       <button className="search-button" type="submit">  {counter}
+       <button className="search-button" type="submit">  Search
        </button>
      </form>
 
-     <h1 onClick={() => setCounter(counter+1)} >{counter}</h1>
     </div>
   );
 };
