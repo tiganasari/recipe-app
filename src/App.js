98cbd6fbac66d2bc7d611f1f4b0f91f9980f4811
Tigana-Sari <tigana.sari@gmail.com>
2021-09-10 19:51:01 +0100

    File structure - react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+
+  const APP_ID = '0346e7d0';
+  const APP_KEY = 'b087565973e9e485c22c11879174743c';
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    console.log('effect has been run')
+  })
+
+  const[counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <form className="search-form">
+       <input className="search-bar" type="text"></input>
+       <button className="search-button" type="submit">  {counter}
+       </button>
+     </form>
+
+     <h1 onClick={() => setCounter(counter+1)} >{counter}</h1>
     </div>
   );
-}
+};
 
 export default App;
